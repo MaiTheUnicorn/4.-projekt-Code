5de6ce6ed985731bef257858f43518ff54de8b67
MaiTheUnicorn <mai.bkroner@gmail.com>
2023-05-02 20:35:47 +0200

    Continue

--- app.js
+++ app.js
@@ -17,3 +17,9 @@ function toggleMenu() {
 }
 
 hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
